0059a8e308e3e8198cd84b4bb49dad0c02e8395b
Anthony Fabius <afabius05@gmail.com>
2023-03-31 20:37:59 -0400

    Tab Bar Test
    Testing out tab bars.

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -9,12 +9,50 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('No shot.'),
-        ),
-      ),
+    return const MaterialApp(home: MainAppBar());
+  }
+}
+
+class MainAppBar extends StatelessWidget {
+  const MainAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text("RPI Food Finder"),
+            bottom: const TabBar(tabs: <Widget>[
+              Tab(
+                text: "Commons",
+              ),
+              Tab(
+                text: "Sage",
+              ),
+              Tab(
+                text: "BARH",
+              ),
+              Tab(
+                text: "Blitman",
+              )
+            ]),
+          ),
+          body: const TabBarView(children: <Widget>[
+            Center(
+              child: Text("Welcome to Commons Dining Hall!"),
+            ),
+            Center(
+              child: Text("Welcome to Russel Sage Hall!"),
+            ),
+            Center(
+              child: Text("Welcome to BARH Dining Hall!"),
+            ),
+            Center(
+              child: Text("Welcome to Blitman Dining Hall!"),
+            ),
+          ])),
     );
   }
 }

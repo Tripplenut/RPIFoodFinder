a21a786a96fb01bcf91a2ea472e128dd1d1f29eb
Anthony Fabius <afabius05@gmail.com>
2023-04-02 17:08:07 -0400

    Drawer and drawer buttons
    Added a drawer that leads to the menus, about, and settings. They aren't interactive yet but I'll get there.

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -9,51 +9,84 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: AppHome());
+    return MaterialApp(
+      home: const AppHome(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.red[300],
+      ),
+    );
   }
 }
 
 class AppHome extends StatelessWidget {
   const AppHome({super.key});
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 0,
       length: 4,
       child: Scaffold(
-          appBar: AppBar(
-            title: const Center(
-              child: Text("RPI Food Finder"),
-            ),
-            bottom: const TabBar(tabs: <Widget>[
-              Tab(
-                text: "Commons",
-              ),
-              Tab(
-                text: "Sage",
-              ),
-              Tab(
-                text: "BARH",
-              ),
-              Tab(
-                text: "Blitman",
-              )
-            ]),
+        appBar: AppBar(
+          title: const Center(
+            child: Text("RPI Food Finder"),
           ),
-          body: const TabBarView(children: <Widget>[
-            Center(
-              child: Text("Welcome to Commons Dining Hall!"),
+          bottom: const TabBar(tabs: <Widget>[
+            Tab(
+              text: "Commons",
             ),
-            Center(
-              child: Text("Welcome to Russel Sage Hall!"),
+            Tab(
+              text: "Sage",
             ),
-            Center(
-              child: Text("Welcome to BARH Dining Hall!"),
+            Tab(
+              text: "BARH",
             ),
-            Center(
-              child: Text("Welcome to Blitman Dining Hall!"),
+            Tab(
+              text: "Blitman",
+            )
+          ]),
+        ),
+        drawer: Drawer(
+          child: ListView(children: const <Widget>[
+            ListTile(
+              leading: IconButton(
+                onPressed: null,
+                icon: Icon(Icons.menu_book),
+              ),
+              title: Text('Menus'),
             ),
-          ])),
+            ListTile(
+              leading: IconButton(
+                onPressed: null,
+                icon: Icon(Icons.info),
+              ),
+              title: Text('About'),
+            ),
+            ListTile(
+              leading: IconButton(
+                onPressed: null,
+                icon: Icon(Icons.settings),
+              ),
+              title: Text('Settings'),
+            ),
+          ]),
+        ),
+        body: const TabBarView(children: <Widget>[
+          Center(
+            child: Text("Welcome to Commons Dining Hall!"),
+          ),
+          Center(
+            child: Text("Welcome to Russel Sage Hall!"),
+          ),
+          Center(
+            child: Text("Welcome to BARH Dining Hall!"),
+          ),
+          Center(
+            child: Text("Welcome to Blitman Dining Hall!"),
+          ),
+        ]),
+      ),
     );
   }
 }

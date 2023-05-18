6048ee8c016b0c01593de47d94b764e50e210dfe
Anthony Fabius <afabius05@gmail.com>
2023-05-17 21:46:02 -0400

    About Page URLS
    Added url launcher and functions to the about page. When ready just plug in links at the top.

--- src/lib/about_page.dart
+++ src/lib/about_page.dart
@@ -1,13 +1,31 @@
 // Flutter Files
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform; // Allows us to see users platform
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart'; // Allows for links on tap
 import 'package:ionicons/ionicons.dart'; // Custom Icons
 // Search for Icons here - https://ionic.io/ionicons/
 
 // Our Files
 import 'reusable_widgets.dart';
+
+// Urls Used in About Page
+
+final Uri githubUrl =
+    Uri.parse('https://github.com/RPI-Food-Finder/RPIFoodFinder');
+//! When Discord is made add invite link on tap
+final Uri discordUrl = Uri.parse('');
+//! When deployed to distributors add review link
+final Uri playstoreUrl = Uri.parse('');
+final Uri appstoreUrl = Uri.parse('');
+final Uri rcosUrl = Uri.parse('https://new.rcos.io/');
+
+// Url Launcher
+Future<void> _launchUrl(Uri url) async {
+  if (!await launchUrl(url)) {
+    throw Exception('Could not launch url');
+  }
+}
 
 // The About Page and all its wonderful contents
 class AboutPage extends StatefulWidget {
@@ -38,21 +56,25 @@ class _AboutPageState extends State<AboutPage> {
           padding: EdgeInsets.only(bottom: 20, right: 30, left: 30),
           child: Center(child: Text(placeholderText)),
         ),
-        const ListTile(
+        ListTile(
           // Github Repo
-          contentPadding: EdgeInsets.all(20),
-          leading: Icon(Ionicons.logo_github),
-          title: Text("Github Repository"),
-          subtitle: Text("Click me!"),
-          onTap: null, //! Add main Github repo when ready
+          contentPadding: const EdgeInsets.all(20),
+          leading: const Icon(Ionicons.logo_github),
+          title: const Text("Github Repository"),
+          subtitle: const Text("Click me!"),
+          onTap: () async {
+            await _launchUrl(githubUrl);
+          },
         ),
-        const ListTile(
+        ListTile(
           // Discord
-          contentPadding: EdgeInsets.all(20),
-          leading: Icon(Ionicons.logo_discord),
-          title: Text("Join our Discord"),
-          subtitle: Text("Click me!"),
-          onTap: null, //! When Discord is made add invite link on tap
+          contentPadding: const EdgeInsets.all(20),
+          leading: const Icon(Ionicons.logo_discord),
+          title: const Text("Join our Discord"),
+          subtitle: const Text("Click me!"),
+          onTap: () async {
+            await _launchUrl(discordUrl);
+          },
         ),
         ListTile(
           // Rate Us!
@@ -64,15 +86,21 @@ class _AboutPageState extends State<AboutPage> {
                   : (const Icon(Ionicons.star)),
           title: const Text("Rate us!"),
           subtitle: const Text("Click me!"),
-          onTap: null, //! When deployed to distributors add review link
+          onTap: () async {
+            (Platform.isIOS)
+                ? (await _launchUrl(appstoreUrl))
+                : (await _launchUrl(playstoreUrl));
+          },
         ),
-        const ListTile(
+        ListTile(
           // RCOS
-          contentPadding: EdgeInsets.only(top: 20, left: 20, right: 20),
+          contentPadding: const EdgeInsets.only(top: 20, left: 20, right: 20),
           // leading: Icon(), //! Add RCOS logo as svg icon and add as an icon
-          title: Text("A Rensselaer Center for Open Source Project"),
-          subtitle: Text("Click me!"),
-          onTap: null, //! Add https link to new.rcos.io
+          title: const Text("A Rensselaer Center for Open Source Project"),
+          subtitle: const Text("Click me!"),
+          onTap: () async {
+            await _launchUrl(rcosUrl);
+          },
         )
       ]),
     );
